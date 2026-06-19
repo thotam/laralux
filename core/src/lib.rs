b3f8cee882a6de2440889903d3bb3cb1a585be83
thotam <thanhtamtqno1@gmail.com>
2026-06-19 07:35:33 +0700

    feat(core): add apt_install and pkexec privileged escalation

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -18,7 +18,7 @@ pub use paths::LaragonPaths;
 pub use process::RealSpawner;
 pub use service::registry::build_services;
 pub use service::ServiceKind;
-pub use privileged::{Privileged, SudoPrivileged};
+pub use privileged::{PkexecPrivileged, Privileged, SudoPrivileged};
 pub use sites::{scan_sites, Site};
 pub use ssl::MkcertIssuer;
 pub use sync::sync_sites;
